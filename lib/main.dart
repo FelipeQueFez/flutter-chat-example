449fb9072f735d27ecae59db237cf83838abf1db
Felipe Silva <felipe.augustotm@gmail.com>
2019-12-25 22:18:51 -0300

    change title

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ExemploFirebase(title: 'Demo Restalks'),
+      home: ExemploFirebase(title: 'Demo Chat'),
     );
   }
 }
